ff4b955234e30c3ab67c54f2f7a897555f7332a7
rajanmah <146156379+rajanmah@users.noreply.github.com>
2023-10-18 23:17:30 -0400

    initial framework

---
+++ app.js
@@ -0,0 +1,141 @@
+// The provided course information.
+const courseInfo = {
+    id: 451,
+    name: "Introduction to JavaScript"
+  };
+  
+  // The provided assignment group.
+  const assignmentGroup = {
+    id: 12345,
+    name: "Fundamentals of JavaScript",
+    course_id: 451,
+    group_weight: 25,
+    assignments: [
+      {
+        id: 1,
+        name: "Declare a Variable",
+        due_at: "2023-01-25",
+        points_possible: 50
+      },
+      {
+        id: 2,
+        name: "Write a Function",
+        due_at: "2023-02-27",
+        points_possible: 150
+      },
+      {
+        id: 3,
+        name: "Code the World",
+        due_at: "3156-11-15",
+        points_possible: 500
+      }
+    ]
+  };
+  
+  // The provided learner submission data.
+  const learnerSubmissions = [
+    {
+      learner_id: 125,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 47
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-02-12",
+        score: 150
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 3,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 400
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-24",
+        score: 39
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-03-07",
+        score: 140
+      }
+    }
+  ];
+
+  // OUTPUT
+//   [
+//     {
+//     id: ID of the learner 125/132
+//     avg:score1+score2/possible-points1+possible-points2 in PERCENTAGE
+//     assignment_id:{ 
+//     id:1,2;
+//     percentage scored : %%
+//      }
+//      }
+//     {
+//     id:
+//     avg:
+//     assignment_id:
+//   }
+    
+//   ]
+//OUTPUT
+
+// function getLearnerData(courseInfo,assignmentGroup,[LearnerSubmission]){
+
+
+// function getLearnerData(courseInfo, assignmentGroup, learnerSubmissions){
+// learnerSubmissions.forEach((element)=>console.log(element));
+
+
+// }
+
+// destructuring Object
+function displayCourseId(assignmentGroup){
+    console.log(assignmentGroup.assignments[1][2]);
+}
+displayCourseId(assignmentGroup)
+
+// console.log(getLearnerData(courseInfo, assignmentGroup, learnerSubmissions));
+
+
+
+  
+//   function getLearnerData(course, ag, submissions) {
+//     // here, we would process this data to achieve the desired result.
+//     const result = [
+//       {
+//         id: 125,
+//         avg: 0.985, // (47 + 150) / (50 + 150)
+//         1: 0.94, // 47 / 50
+//         2: 1.0 // 150 / 150
+//       },
+//       {
+//         id: 132,
+//         avg: 0.82, // (39 + 125) / (50 + 150)
+//         1: 0.78, // 39 / 50
+//         2: 0.833 // late: (140 - 15) / 150
+//       }
+//     ];
+  
+//     return result;
+//   }
+  
+//   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+  
+//   console.log(result);
+  
